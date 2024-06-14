d670bc941d5eef2b5086e9b996983fbdd09baa39
Faizan Nauman Shaikh <140354659+faizanns@users.noreply.github.com>
2024-06-14 07:41:20 +0200

    Update messages.jsx
    testing correct connnection with tutor setup

--- src/register/messages.jsx
+++ src/register/messages.jsx
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'register.page.title': {
     id: 'register.page.title',
-    defaultMessage: 'Register | {siteName}',
+    defaultMessage: 'Register Icecream | {siteName}',
     description: 'register page title',
   },
   // Field labels
